7571522d64cda55179c15a17c93868b7805d4b01
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-04-08 20:21:58 +0800

    fix: 返回 json 处理

--- model/vo/order.go
+++ model/vo/order.go
@@ -3,13 +3,13 @@ package vo
 import "github.com/ncuhome/story-cook/model/dao"
 
 type OrderResp struct {
-	OrderId uint   `json:"order_id "`
-	TaskId  uint   `json:"task_id "`
-	StoryId uint   `json:"story_id "`
-	Comment string `json:"comment "`
-	Score   int    `json:"score "`
-	Money   int    `json:"money "`
-	Status  int    `json:"status "` // 0:未完成 1:进行中 2:已完成
+	OrderId uint   `json:"order_id"`
+	TaskId  uint   `json:"task_id"`
+	StoryId uint   `json:"story_id"`
+	Comment string `json:"comment"`
+	Score   int    `json:"score"`
+	Money   int    `json:"money"`
+	Status  int    `json:"status"` // 0:未完成 1:进行中 2:已完成
 }
 
 func BuildOrderResp(order *dao.Orders) *OrderResp {
